4a1eed7ac7747c5f6bcc7313ce80e3d4bdb6a0c0
Michel Jouaux <micheljouaux@gmx.de>
2024-04-01 22:51:48 +0200

    Add: Game Over Screen

--- Arkanoid/Assets/Scripts/GameManager.cs
+++ Arkanoid/Assets/Scripts/GameManager.cs
@@ -1,19 +1,26 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public Sprite[] healthBallSprites = new Sprite[3];
     public Image[] healthBallImages = new Image[3];
-    public TextMeshProUGUI highScoreNumber;
+    public TextMeshProUGUI highScoreNumber_InGame;
+    public TextMeshProUGUI highScoreNumber_GameEnd;
+    public Canvas gameEndScreen;
+    public TextMeshProUGUI titleGameEndScreen;
 
     public int Score { get; set; }
 
     public int Lives { get; set; } = 3;
+
     // Start is called before the first frame update
     void Start()
     {
+        gameEndScreen.enabled = false;
+        
         foreach (Image image in healthBallImages)
         {
             image.sprite = healthBallSprites[2];
@@ -23,7 +30,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        highScoreNumber.text = string.Format("{0:N}", Score.ToString());
+        highScoreNumber_InGame.text = string.Format("{0:N}", Score.ToString());
 
         switch (Lives)
         {
@@ -32,6 +39,7 @@ public class GameManager : MonoBehaviour
                 {
                     image.sprite = healthBallSprites[2];
                 }
+
                 break;
             case 2:
                 healthBallImages[0].enabled = false;
@@ -43,8 +51,21 @@ public class GameManager : MonoBehaviour
                 healthBallImages[1].enabled = false;
                 healthBallImages[2].sprite = healthBallSprites[0];
                 break;
-            // default:
-            //     break;
+            case 0:
+                highScoreNumber_GameEnd.text = string.Format("{0:N}", Score.ToString());
+                titleGameEndScreen.text = "Game Over";
+                gameEndScreen.enabled = true;
+                break;
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Scenes/GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
